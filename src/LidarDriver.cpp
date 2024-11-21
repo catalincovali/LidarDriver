92ad4ae302346e07b6ddc14b89ddbe836c748355
catalincovali <catalincovali@icloud.com>
2024-11-21 20:03:17 +0100

    formatting

---
+++ src/LidarDriver.cpp
@@ -0,0 +1,31 @@
+#inlcude <iostream>
+#inlcude <ostream>
+#inlcude "LidarDrvier.h"
+
+
+//constructr()
+
+
+
+
+//new_size()
+
+
+
+
+//get_scan()
+
+
+
+
+//clear_buffer()
+
+
+
+
+//get_distance()
+
+
+
+
+//operator<<
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <ostream>
+#include <vector>
+
+int main() {
+
+  return 0;
+}
