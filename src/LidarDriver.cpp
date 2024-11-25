405ade4d1d6f6c8a0ec1b5942624b4ca776f21fd
Kistina Gavrilovska <kistinagavrilovska@192.168.1.41>
2024-11-25 18:43:32 +0100

    first commit

--- src/LidarDriver.cpp
+++ src/LidarDriver.cpp
@@ -1,10 +1,24 @@
-#include <iostream>
-#include <ostream>
+
 #include "LidarDriver.h"
 
 
 
 //construct()
+LidarDriver::LidarDriver() :buffer_(buffer_dim_, std::vector<double>(numero_letture_))
+{  
+}     // do la dimensione al buffer
+
+
+
+//controllo che la grandezza dell'angolo inserito sia adeguata con un set 
+void LidarDriver:: set_risoluzione_angolo(double r){
+      if ( r>0 && r<1) {
+        risoluzione_angolo_ = r;
+      }
+      else {
+      throw std::out_of_range(" la risoluzione dell'angolo non è valida")
+      }
+}
 
 
 
