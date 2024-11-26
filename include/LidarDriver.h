4a0857576d8b1b32b28275a184538624b800c009
Kistina Gavrilovska <kistinagavrilovska@192.168.1.41>
2024-11-26 16:38:36 +0100

    compilabile

--- include/LidarDriver.h
+++ include/LidarDriver.h
@@ -5,6 +5,7 @@
 #include <vector>
 #include <cmath>
 #include <stdexcept>
+
 class LidarDriver {
 public:
 
@@ -12,36 +13,37 @@ public:
 LidarDriver();
 
 
-//set risoluzione angolo
+/*  funzione di set della risoluzione angolo che controlla che l'angolo
+inserito sia corretto in alternativa lancia l'eccezione out_of_range   */
 void set_risoluzione_angolo(double r);
 
 
-//newscan
+//new_scan gestirà le parti del buffer dove scrivere o sovrascrivere il
+nuovo vettore
 void new_scan(std::vector<double> ns);
 
-//funzione che userò nel setter
+//funzione gestirà il vettore da inserire nel new_scan
 void scrivi_buffer(std::vector<double> sb);
 
 
 private:
 //creo il buffer
-std::vector<std::vector<double>> buffer_; 
+std::vector<std::vector<double>> buffer_;
 
 const int BUFFER_DIM_=10;
 
 /*
-numero letture mi indica il numero di letture dopo aver dato un angolo
-utilizzo la funzione round per avere un numero intero come dimensione del 
-vettore
+numero letture viene inizializzato nella funzione setter dell'angolo, mi
+indica il numero di letture calcolato in base all'angolo inserito nel 
+setter
 */
-static int numero_letture_; 
-//deve essere un numero intero ( const perche così verrà letto in lettura e non compilazione )
+int numero_letture_; 
 
 //risoluzione angolo che verrà inizializzata successivamente dalla 
 funzione setter
-static  double risoluzione_angolo_ ;
+double risoluzione_angolo_;
 
-static constexpr int angolo_max_=180;
+const int angolo_max_=180;
 
 
 };
