ab193f305eaff9d957fe18daf060e852028e29d8
kiki30072004 <gavrilovskakristina2004@gmail.com>
2024-11-27 18:46:16 +0100

    Update LidarDriver.h

--- include/LidarDriver.h
+++ include/LidarDriver.h
@@ -1,41 +1,42 @@
 #ifndef LIDARDRIVER_H
 #define LIDARDRIVER_H
+#include <cmath>
 #include <iostream>
 #include <ostream>
+#include <stdexcept> //for std::out_of_range
 #include <vector>
-#include <cmath>
-#include <stdexcept>
 
 class LidarDriver {
-public:
-
-//costruttore
-LidarDriver();
-
-//costruttore con angolo
-LidarDriver(double a);
-
-
-//new_scan gestirà le parti del buffer dove scrivere o sovrascrivere il
-
-void new_scan(std::vector<double> ns);
-
-//funzione gestirà il vettore da inserire nel new_scan
-void scrivi_buffer(std::vector<double> sb);
-
-
 private:
-bool isEmpty;
-int first;//mi indica il primo posto del buffer libero per un nuovo scan
-int last;//mi indica lo scan più vecchio di tutti
-std::vector<std::vector<double>> buffer_;//creo il buffer di double
+  std::vector<std::vector<double>> buffer_;
+  int first;
+  int last;
 
-const int BUFFER_DIM_=4;
+  const int BUFFER_DIM_=4;
+  const double DEF_ANG_=1.0;
+  const int ANGOLO_MAX_ = 10;
 
-int numero_letture_;
-double risoluzione_angolo_;
-const double DEF_ANG_=1.0;
+  int numero_letture_;
+  double risoluzione_angolo_;
+  bool isEmpty;
 
-const int ANGOLO_MAX_ = 180;
+public:
+  LidarDriver();
+  LidarDriver(double a);
+ 
+  void new_scan(std::vector<double> ns);
+  void scrivi_buffer(std::vector<double> sb);
+
+  //get_scan()
+  std::vector<double> get_scan();
+  //clear_buffer()
+  void clear_buffer();
+
+  double get_distance(double angle);
+std::vector<double> get_last(void);
+
 };
+
+std::ostream& operator<<(std::ostream& os, LidarDriver& v);
+
 #endif
