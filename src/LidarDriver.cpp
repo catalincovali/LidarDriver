6d9d2447d43e827917e4bbc7374c690bbd7179e5
LucaBortolazzi <luca.bortolazzi@studenti.unipd.it>
2024-11-27 13:11:29 +0100

    Update LidarDriver.cpp

--- src/LidarDriver.cpp
+++ src/LidarDriver.cpp
@@ -9,9 +9,33 @@
 
 //constructr()
 
+LidarDriver::LidarDriver()
+    : risoluzione_angolo_(DEF_ANG_),  // Imposta un valore predefinito
+      numero_letture_(static_cast<int>(std::round(ANGOLO_MAX_ / risoluzione_angolo_))),
+      buffer_(BUFFER_DIM_, std::vector<double>(numero_letture_)), first(-1), last(0) 
+{
+  numero_letture_ = static_cast<int>(std::round(ANGOLO_MAX_ / risoluzione_angolo_))+1;
+}
 
 
 
+
+//constructor(angRes)
+LidarDriver::LidarDriver(double a)
+    :risoluzione_angolo_{a},
+      numero_letture_(static_cast<int>(std::round(ANGOLO_MAX_ / risoluzione_angolo_))),
+      buffer_(BUFFER_DIM_, std::vector<double>(numero_letture_)),first(-1), last(0) 
+{
+  isEmpty=true;
+  if(a<0 || a>1)  
+    throw std::out_of_range(" la risoluzione dell'angolo non è valida");
+
+      
+  numero_letture_ = static_cast<int>(std::round(ANGOLO_MAX_/risoluzione_angolo_))+1;
+}
+      
+      
+      
 //new_scan()
 
 
@@ -33,9 +57,9 @@ double LidarDriver::get_distance(double angle){
 	if(angle<0 || angle>180)
 		throw std::out_of_range("l'angolo deve essere compreso tra 0 e 180");
 	
-	double tempIndex = angle/risoluzione_angolare;
+	double tempIndex = angle/risoluzione_angolo_;
 	int index = static_cast<int>( std::round(tempIndex) );
-	double lettura = v.at(last-1).at(index);
+	double lettura = buffer_.at(last-1).at(index);
 	
 	return lettura;
 }
@@ -43,19 +67,15 @@ double LidarDriver::get_distance(double angle){
 
 
 //operator<<
-std::ostream& operator<<(std::ostream& os, std::vector<std::vector<double>>& v){
+std::ostream& operator<<(std::ostream& os, LidarDriver& v){
 	os << "[";
-	for(int i=0; i < v.size(); i++){
-		os << v.at(v.get_last()-1).at(i);
-		if(i != v.size()-1) os << ", ";		
+	std::vector<double> myVec = v.get_last(); 
+	for(int i=0; i < myVec.size(); i++){
+		os << myVec.at(i);
+		if(i != myVec.size()-1) os << ", ";		
 	}
 	os << "]";
 	return os;
 }
 
-
-//get_last()
-int LidarDriver::get_last(){
-	return last;
-}
 
