67f96505515878d55428e12160ccc12444d57720
LucaBortolazzi <luca.bortolazzi@studenti.unipd.it>
2024-11-27 13:10:46 +0100

    Update LidarDriver.h

--- include/LidarDriver.h
+++ include/LidarDriver.h
@@ -6,14 +6,28 @@
 
 class LidarDriver {
 private:
+std::vector<std::vector<double>> buffer_;
+  int first;
+  int last;
 
+  const int BUFFER_DIM_=4;
+  const double DEF_ANG_=1.0;
+  const int ANGOLO_MAX_ = 180;
+
+  int numero_letture_;
+  double risoluzione_angolo_;
+  bool isEmpty;
 
 
 public:
+
+  LidarDriver();
+  LidarDriver(double a);
+
 	double get_distance(double angle);
-	int get_last();
+	std::vector<double> get_last(void) {return buffer_.at(last);}
 };
 
-std::ostream& operator<<(std::ostream& os, std::vector<std::vector<double>> v);
+std::ostream& operator<<(std::ostream& os, LidarDriver& v);
 
 #endif
